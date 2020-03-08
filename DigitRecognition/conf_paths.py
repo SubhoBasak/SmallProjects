a09cfa6cac77db04547288a3ce953912b7cccdee
SubhoBasak <subhobasak50@gmail.com>
2020-03-08 17:00:30 +0530

    update the project

--- DigitRecognition/conf_paths.py
+++ DigitRecognition/conf_paths.py
@@ -5,6 +5,8 @@ icon_num = random.randint(1, 4)
 
 __FILE_DIR__ = os.path.dirname(__file__).replace('\\', '/')
 
-__CLASSIFIER__ = os.path.join(__FILE_DIR__, 'classifier/forest_clf.pkl').replace('\\', '/')
+__CLASSIFIER_1__ = os.path.join(__FILE_DIR__, 'classifier/digit_clf1.pkl').replace('\\', '/')
+__CLASSIFIER_2__ = os.path.join(__FILE_DIR__, 'classifier/digit_clf2.pkl').replace('\\', '/')
+__SCALER__ = os.path.join(__FILE_DIR__, 'classifier/scaler.pkl').replace('\\', '/')
 
 __ICON__ = os.path.join(__FILE_DIR__, 'img_png/icons/icon_'+str(icon_num)+'.png').replace('\\', '/')
--- DigitRecognition/digit_recognition.py
+++ DigitRecognition/digit_recognition.py
@@ -6,11 +6,10 @@ import numpy as np
 import tkinter as tk
 import pyttsx3
 import conf_paths as cf
-from PIL import Image, ImageTk, ImageDraw
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
+from PIL import Image, ImageDraw
+# import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# from sklearn.preprocessing import MinMaxScaler
 from tkinter import messagebox
 
 class MainWindow:
@@ -19,6 +18,9 @@ class MainWindow:
         self.old_x = None
         self.old_y = None
         self.draw = False
+
+        self.clf = tk.IntVar()
+        self.clf.set(1)
 
         self.sound_var = tk.IntVar()
         self.sound_var.set(1)
@@ -36,18 +38,25 @@ class MainWindow:
         self.sound_menu.add_radiobutton(label = 'On', variable = self.sound_var, value = True)
         self.sound_menu.add_radiobutton(label = 'Off', variable = self.sound_var, value = False)
 
+        self.clf_menu = tk.Menu(self.root, tearoff = False)
+        self.main_menu.add_cascade(label = 'Classifier', menu = self.clf_menu)
+        self.clf_menu.add_radiobutton(label = 'Classifier 1', variable = self.clf, value = 1)
+        self.clf_menu.add_radiobutton(label = 'Classifier 2', variable = self.clf, value = 2)
+
         self.output_label = tk.Label(self.root)
         self.output_label.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
         self.output_label.config(bg = '#adcaf7')
 
-        self.input_canvas = tk.Canvas(self.root, width = 100, height = 100)
+        self.input_canvas = tk.Canvas(self.root, width = 80, height = 80)
         self.input_canvas.pack(side = tk.BOTTOM, fill = tk.BOTH, expand = True)
         self.input_canvas.config(bg = '#bfd4f5')
         self.input_canvas.bind('<B1-Motion>', self.paint)
         self.input_canvas.bind('<ButtonRelease-1>', self.reset)
 
         self.root.update()
-        self.classifier = joblib.load(cf.__CLASSIFIER__)
+        self.classifier1 = joblib.load(cf.__CLASSIFIER_1__)
+        self.classifier2 = joblib.load(cf.__CLASSIFIER_2__)
+        self.scaler = joblib.load(cf.__SCALER__)
         self.engine = pyttsx3.init()
 
         self.tmp_img = Image.new('L', (self.input_canvas.winfo_width(), self.input_canvas.winfo_height()), 'black')
@@ -58,8 +67,8 @@ class MainWindow:
             self.top.destroy()
         if self.old_x != None:
             self.input_canvas.create_line(self.old_x, self.old_y, event.x, event.y,
-                                          width = 15, smooth = True, capstyle = 'round')
-            self.tmp_draw.line([self.old_x, self.old_y, event.x, event.y], 'white', 15)
+                                          width = 35, smooth = True, capstyle = 'round')
+            self.tmp_draw.line([self.old_x, self.old_y, event.x, event.y], 'white', 30)
             self.draw = True
         self.old_x = event.x
         self.old_y = event.y
@@ -71,21 +80,21 @@ class MainWindow:
         if self.draw == False:
             return None
 
-        self.tmp_img = self.tmp_img.resize((28, 28))
+        self.tmp_img = self.tmp_img.resize((8, 8))
         self.tmp_img = np.array(self.tmp_img)
         self.predict_digit()
 
-        self.top = tk.Toplevel()
-        self.top.geometry('200x200+100+100')
-
-        fig = plt.figure()
-        axs = fig.add_subplot(1, 1, 1)
-
-        axs.imshow(self.tmp_img.ravel().reshape(28, 28))
-
-        fig_cnv = FigureCanvasTkAgg(fig, self.top)
-        widget = fig_cnv.get_tk_widget()
-        widget.pack()
+        # self.top = tk.Toplevel()
+        # self.top.geometry('200x200+100+100')
+        #
+        # fig = plt.figure()
+        # axs = fig.add_subplot(1, 1, 1)
+        #
+        # axs.imshow(self.tmp_img.ravel().reshape(8, 8))
+        #
+        # fig_cnv = FigureCanvasTkAgg(fig, self.top)
+        # widget = fig_cnv.get_tk_widget()
+        # widget.pack()
 
         self.tmp_img = Image.new('L', (self.input_canvas.winfo_width(), self.input_canvas.winfo_height()), 'black')
         self.tmp_draw = ImageDraw.Draw(self.tmp_img)
@@ -93,10 +102,13 @@ class MainWindow:
 
 
     def predict_digit(self):
-        x = self.classifier.predict_proba([self.tmp_img.ravel()])[0]
+        self.tmp_img = self.scaler.transform([self.tmp_img.ravel()])
+        if self.clf.get() == 1:
+            x = self.classifier1.predict_proba(self.tmp_img)[0]
+        else:
+            x = self.classifier2.predict_proba(self.tmp_img)[0]
         indx = x.argmax()
-
-        if x[indx] < 0.6:
+        if x[indx] < 0.2:
             self.output_label.config(text = '')
             if self.sound_var.get() == 1:
                 task = threading.Thread(target = self.not_found)
@@ -104,53 +116,35 @@ class MainWindow:
             messagebox.showerror('Not recognized',
                                  'I can\'t recognized what you write, Plese make the image more clear.')
             return None
-        if indx == 0:
-            self.output_label.config(text = '0', font = ('Arial', 30))
-        elif indx == 1:
-            self.output_label.config(text = '1', font = ('Arial', 30))
-        elif indx == 2:
-            self.output_label.config(text = '2', font = ('Arial', 30))
-        elif indx == 3:
-            self.output_label.config(text = '3', font = ('Arial', 30))
-        elif indx == 4:
-            self.output_label.config(text = '4', font = ('Arial', 30))
-        elif indx == 5:
-            self.output_label.config(text = '5', font = ('Arial', 30))
-        elif indx == 6:
-            self.output_label.config(text = '6', font = ('Arial', 30))
-        elif indx == 7:
-            self.output_label.config(text = '7', font = ('Arial', 30))
-        elif indx == 8:
-            self.output_label.config(text = '8', font = ('Arial', 30))
-        elif indx == 9:
-            self.output_label.config(text = '9', font = ('Arial', 30))
-
+        self.make_output(indx)
         if self.sound_var.get() == 1:
             task = threading.Thread(target = self.speak, args = [indx, x[indx]])
             task.start()
+
+    def make_output(self, val):
+        self.output_label.config(text=str(val), font=('Arial', 30))
 
     def not_found(self):
         try:
             self.engine.say('Sorry, but I can\'t recognized what you write, Plese make the image more clear.')
             self.engine.runAndWait()
         except RuntimeError:
-            time.sleep(1000)
-            self.not_found()
+            time.sleep(500)
 
     def speak(self, val, prob):
-        if prob > 0.9:
+        if prob > 0.8:
             speech = random.sample(['Ohh, It is ', 'I am sure it is '], 1)
             speech += str(val)
             self.engine.say(speech)
-        elif prob > 0.8:
+        elif prob > 0.6:
             speech = random.sample(['I think it is ', 'eighty percent sure it is '], 1)
             speech += str(val)
             self.engine.say(speech)
-        elif prob > 0.7:
+        elif prob > 0.4:
             speech = random.sample(['It is very close to ', 'There is little bit of confusion, but may be it is '], 1)
             speech += str(val)
             self.engine.say(speech)
-        elif prob > 0.6:
+        elif prob > 0.2:
             speech = random.sample(['Not sure, but looking like ', 'May be I am wrong, but it is look like '], 1)
             speech += str(val)
             self.engine.say(speech)
@@ -159,7 +153,6 @@ class MainWindow:
         except RuntimeError:
             time.sleep(2000)
             self.speak(val, prob)
-
 
 if __name__ == '__main__':
     root = tk.Tk()
